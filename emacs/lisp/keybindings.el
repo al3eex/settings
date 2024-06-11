54bbc180c367c67023e18ad92f69834c89112728
al3eex <alexz1243421@gmail.com>
2024-06-11 15:26:39 +0300

    removed emacs

--- emacs/lisp/keybindings.el
+++
@@ -1,8 +0,0 @@
-;;; keybindings.el --- setup keybindings (suggestions, guides, comfort) -*- lexical-binding: t; -*-
-
-;;; Code:
-(use-package which-key
-  :config (which-key-mode))
-
-(provide 'keybindings)
-;;; keybindings.el ends here
